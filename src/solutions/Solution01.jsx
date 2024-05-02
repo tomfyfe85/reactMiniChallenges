1b5185b33241344831a322e242b6f737ea02e32e
Tom Fyfe <tomfyfe85@gmail.com>
2024-05-02 16:19:38 +0100

    solution 1 complete

--- src/solutions/Solution01.jsx
+++ src/solutions/Solution01.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
 const Solution01 = () => {
-  return <div>ADD YOUR SOLUTION HERE</div>;
+  const [count, setCount] = useState(0);
+
+  const add = () => {
+    setCount((prevCountAdd1) => prevCountAdd1 + 1);
+  };
+
+  const subtract = () => {
+    setCount((prevCountSubtract1) => prevCountSubtract1 - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={add} type="add">
+        add 1
+      </button>
+      <br />
+      <p>{count}</p>
+      <button onClick={subtract} type="subtract">
+        subtract 1
+      </button>
+    </div>
+  );
 };
 
 export default Solution01;
