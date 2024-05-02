70913e49b05fd8a6d50ccc93ea1e24b026701652
Tom Fyfe <tomfyfe85@gmail.com>
2024-05-02 23:27:32 +0100

    handle submit creates an object

--- src/solutions/Solution02.jsx
+++ src/solutions/Solution02.jsx
@@ -1,24 +1,28 @@
 import { useState } from "react";
 
-function Todo({id, task}) {
+function Todo({ id, task }) {
   return (
     <>
-      
       <p>{task}</p>
+      <button>DELETE</button>
+      <br />
     </>
-  )
+  );
 }
 
 export default function Solution02() {
   const [text, setText] = useState("");
   const [todo, setTodo] = useState([]);
+  const [id, setId] = useState(0);
 
   function handleSubmit(e) {
     e.preventDefault();
-    todo.push(text);
+    setId((prevIdAdd1) => prevIdAdd1 + 1);
 
-    const newArr = todo;
-    setTodo(newArr); // const newTodos = todo.push(text)
+    let ob = { id: id, task: text };
+
+    todo.push(ob);
+    setTodo(todo); // const newTodos = todo.push(text)
     setText("");
   }
 
@@ -33,12 +37,16 @@ export default function Solution02() {
         <textarea value={text} onChange={handleTextChange} />
         <button type="submit">Submit</button>
       </form>
-      {todo.map((info, index) => (
-        <div key={index}><Todo id={index} task={info} /></div>
+      {todo.map((info) => (
+        <div key={info.id}>
+          <Todo id={info.id} task={info.task} />
+
+          <br />
+        </div>
       ))}
     </div>
   );
-};
+}
 
 // add submit button
 // add input field
