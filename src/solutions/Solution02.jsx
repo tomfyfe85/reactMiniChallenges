df55394da0231b7e77f2c5756c26a0bda8fe1317
Tom Fyfe <tomfyfe85@gmail.com>
2024-05-02 17:36:50 +0100

    working on chall 2. Need to sort out input

--- src/solutions/Solution02.jsx
+++ src/solutions/Solution02.jsx
@@ -1,5 +1,28 @@
+import {useState} from 'react'
+
 const Solution02 = () => {
-  return <div>ADD YOUR SOLUTION HERE</div>;
+  const [text, setText] = useState('')
+  const [todo, setTodo] = useState(['hi', 'there', 'newTodo'])
+
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <button type="submit">Submit</button>
+      <input type="todoText" />
+      {todo.map((info, index) => (
+      <div key={index}>
+        {info}
+      </div>
+      ))}
+    </div>
+  );
 };
 
 export default Solution02;
+// add submit button 
+// add input field
+// input field needs state that is updated per event
+// onClick save input as state and create a new todo
+// map through array of todos
+
