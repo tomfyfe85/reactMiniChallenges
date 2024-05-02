1feb95e29588706fa2fce9ff16d67e82102ec0d2
Tom Fyfe <tomfyfe85@gmail.com>
2024-05-02 22:14:28 +0100

    user can add todo's to a list in the browser

--- src/solutions/Solution02.jsx
+++ src/solutions/Solution02.jsx
@@ -1,28 +1,47 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-const Solution02 = () => {
-  const [text, setText] = useState('')
-  const [todo, setTodo] = useState(['hi', 'there', 'newTodo'])
+function Todo({id, task}) {
+  return (
+    <>
+      
+      <p>{task}</p>
+    </>
+  )
+}
 
+export default function Solution02() {
+  const [text, setText] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    todo.push(text);
+
+    const newArr = todo;
+    setTodo(newArr); // const newTodos = todo.push(text)
+    setText("");
+  }
+
+  function handleTextChange(e) {
+    setText(e.target.value);
+  }
 
   return (
     <div>
       <h1>Todo List</h1>
-      <button type="submit">Submit</button>
-      <input type="todoText" />
+      <form onSubmit={handleSubmit}>
+        <textarea value={text} onChange={handleTextChange} />
+        <button type="submit">Submit</button>
+      </form>
       {todo.map((info, index) => (
-      <div key={index}>
-        {info}
-      </div>
+        <div key={index}><Todo id={index} task={info} /></div>
       ))}
     </div>
   );
 };
 
-export default Solution02;
-// add submit button 
+// add submit button
 // add input field
 // input field needs state that is updated per event
 // onClick save input as state and create a new todo
 // map through array of todos
-
