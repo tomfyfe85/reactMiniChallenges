eea4bf8e4835448fffd3c0c3720640805bbe5ad7
Tom Fyfe <tomfyfe85@gmail.com>
2024-05-06 09:12:16 +0100

    delete button works. needs refactoring

--- src/solutions/Solution02.jsx
+++ src/solutions/Solution02.jsx
@@ -2,18 +2,14 @@ import { useState } from "react";
 
 function Todo({ id, task, todo, setTodo }) {
   const deleteTodo = () => {
-    todo.forEach((object) => {
-      if (id === object.id) {
-        todo.splice(id - 1, 1);
-        const newTodo = todo;
-        console.log(newTodo);
-        setTodo(newTodo);
+    const filtered = todo.filter(checkId);
 
-        return;
-      }
-    });
+    function checkId(todos) {
+      return todos.id !== id;
+    }
+    setTodo(filtered);
+
   };
-  console.log(todo);
   return (
     <>
       <p>{task}</p>
@@ -34,14 +30,13 @@ export default function Solution02() {
     setTaskId((prevIdAdd1) => prevIdAdd1 + 1);
     let ob = { id: taskId, task: text };
     todo.push(ob);
-    setTodo(todo); // const newTodos = todo.push(text)
+    setTodo(todo);
     setText("");
   }
 
   function handleTextChange(e) {
     setText(e.target.value);
   }
-  console.log(todo);
 
   return (
     <div>
@@ -50,7 +45,7 @@ export default function Solution02() {
         <textarea value={text} onChange={handleTextChange} />
         <button type="submit">Submit</button>
       </form>
-      {todo.map((info, index) => (
+      {todo.map((info) => (
         <div key={info.id}>
           <Todo id={info.id} task={info.task} todo={todo} setTodo={setTodo} />
         </div>
@@ -58,5 +53,3 @@ export default function Solution02() {
     </div>
   );
 }
-
-// todo={todo} setTodo={setTodo}
